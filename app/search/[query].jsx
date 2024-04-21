d6734fca5e7c50fd86262ca9b0ca83e5e3e32b4f
Samouil <samouilpratziotis@gmail.com>
2024-04-21 14:28:11 +0300

    Final_Bookmarks_not_added(yet!)

---
+++ app/search/[query].jsx
@@ -0,0 +1,67 @@
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useEffect } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import SearchInput from '../../components/SearchInput'
+
+import {images} from '../../constants'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
+
+import { useState } from 'react'
+
+import {getAllPosts, getLatestPosts, searchPosts} from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
+import { useLocalSearchParams } from 'expo-router'
+
+const Search = () => {
+
+  const {query} = useLocalSearchParams();
+
+  const {data: posts, refetch } = useAppwrite(() => searchPosts(query));
+
+  useEffect(() => {
+    refetch()
+  },[query])
+
+  return (
+    <SafeAreaView className="bg-primary  h-full">
+      <FlatList 
+        data={posts}
+        keyExtractor={(item) => item.$id }
+        renderItem={({ item }) => (
+          <VideoCard 
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={()=>(
+          <View className="my-6 px-4">
+            
+            <Text className="font-pmedium text-sm text-gray-100">
+              Search Results
+            </Text>
+            <Text className="text-2xl font-psemibold text-white">
+              {query}
+            </Text>
+            <View className="mt-6 mb-8">
+              <SearchInput initialQuery={query}/>
+            </View>
+
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState 
+            title="No Videos Found"
+            subtitle="No Videos found for this search query"
+          />
+        )}
+      />
+    </SafeAreaView>
+  )
+}
+
+export default Search
